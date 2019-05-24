252db1aa0c474042179279159364dabbb7af6ac1
SE UNRC <fbrusatti@dc.exa.unrc.edu.ar>
2019-05-24 16:33:14 -0300

    Add client app with basic authorization working

---
+++ client/navigation/AppNavigator.js
@@ -0,0 +1,20 @@
+import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+
+import HomeScreen from '../screens/HomeScreen';
+import OtherScreen from '../screens/OtherScreen';
+import SignInScreen from '../screens/SignInScreen';
+import AuthLoadingScreen from '../screens/AuthLoadingScreen';
+
+const AppStack = createStackNavigator({ Home: HomeScreen, Other: OtherScreen });
+const AuthStack = createStackNavigator({ SignIn: SignInScreen });
+
+export default createAppContainer(createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    App: AppStack,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'AuthLoading',
+  }
+));
